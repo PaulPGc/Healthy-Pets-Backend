b369805cd3ab5dfe84a9559a7587974acab01f3f
PaulPGc <garrixero17@gmail.com>
2025-10-12 16:23:29 -0500

    feat: creacion de archivos del proyecto de spring

---
+++ src/test/java/com/HealthyPetsBackend/HealthyPetsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HealthyPetsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyPetsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
